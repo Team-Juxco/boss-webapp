5e3ccd66d9f517f0dbaba1194208c1395b9becab
matthewlroy <mroyucs@outlook.com>
2018-02-16 14:53:05 -0800

    Initial commit for testing YAHOO

---
+++ BOSSMVC5.Tests/BuildUnitTestingTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace BOSSMVC5.Tests
+{
+	[TestClass]
+	public class BuildUnitTestingTest
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+			RemoteWebDriver driver;
+			string url = "http://teamjuxcoboss-env.us-west-1.elasticbeanstalk.com/";
+			driver = new ChromeDriver();
+			driver.Navigate().GoToUrl(url);
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
---
+++ BOSSMVC5.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BOSSMVC5.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BOSSMVC5.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ac5c2b5c-e49d-485c-b16d-c8b85dd1d12c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
