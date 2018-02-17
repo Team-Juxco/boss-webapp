e743328cfa3a144efe2ed40782d3737ec25a8b63
Bianca Aguirre <biancanoelaguirre@gmail.com>
2018-02-17 12:33:51 -0800

    Test Projects with BuildUnitTestingTest and FuelInventoryTest

--- BOSSMVC5.Tests/BuildUnitTestingTest.cs
+++ BOSSMVC5.Tests/BuildUnitTestingTest.cs
@@ -6,19 +6,35 @@ using OpenQA.Selenium.Remote;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
 
+
 namespace BOSSMVC5.Tests
 {
 	[TestClass]
 	public class BuildUnitTestingTest
 	{
-		[TestMethod]
-		public void TestMethod1()
+        // 
+        // Dashboard Navigation Link TestMethods
+        //
+
+        
+
+        [TestMethod]
+		public void DashboardLink()
 		{
 			RemoteWebDriver driver;
 			string url = "http://teamjuxcoboss-env.us-west-1.elasticbeanstalk.com/";
 			driver = new ChromeDriver();
 			driver.Navigate().GoToUrl(url);
-			Console.WriteLine("Hello World!");
+            driver.Manage().Window.Maximize();
+            driver.FindElementByLinkText("Dashboard").Click();
+
+            //driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(4);
+
+
+
+            //driver.Quit();
 		}
-	}
+
+        
+    }
 }
