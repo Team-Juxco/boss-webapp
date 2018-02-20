740f33d50a113cf03067b63989e7290b66f7ba7d
matthewlroy <mroyucs@outlook.com>
2018-02-19 18:10:19 -0800

    Add TestHelper class

--- BOSSMVC5.Tests/BuildUnitTestingTest.cs
+++ BOSSMVC5.Tests/BuildUnitTestingTest.cs
@@ -1,40 +1,23 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using OpenQA.Selenium;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium.Remote;
 using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Support.UI;
-
 
 namespace BOSSMVC5.Tests
 {
+	/// <summary>
+	/// The entry point class to ensure the testing environment is set up properly.
+	/// This class should pass as it only instantiates a TestHelper class and opens the dashboard.
+	/// </summary>
 	[TestClass]
 	public class BuildUnitTestingTest
 	{
-        // 
-        // Dashboard Navigation Link TestMethods
-        //
-
-        
-
         [TestMethod]
-		public void DashboardLink()
+		public void OpenDashboard()
 		{
-			RemoteWebDriver driver;
-			string url = "http://teamjuxcoboss-env.us-west-1.elasticbeanstalk.com/";
-			driver = new ChromeDriver();
-			driver.Navigate().GoToUrl(url);
-            driver.Manage().Window.Maximize();
-            driver.FindElementByLinkText("Dashboard").Click();
-
-            //driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(4);
-
-
-
-            //driver.Quit();
+			TestHelper th = new TestHelper("http://teamjuxcoboss-env.us-west-1.elasticbeanstalk.com/");
+			RemoteWebDriver chromeDriver = th.GetChromeDriver();
+			chromeDriver.Navigate().GoToUrl(th.GetURL());
+			chromeDriver.Quit();
 		}
-
-        
     }
 }
---
+++ BOSSMVC5.Tests/TestHelper.cs
@@ -0,0 +1,38 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+
+namespace BOSSMVC5.Tests
+{
+	class TestHelper
+	{
+
+		/// <summary>
+		/// The link for the page being tested
+		/// </summary>
+		private string URL;
+		public string GetURL() { return URL; }
+		public void SetURL(string URL) { this.URL = URL; }
+
+		/// <summary>
+		/// Create a new TestHelper object to drive the Selenium testing with reduced redundancy 
+		/// </summary>
+		/// <param name="URL">The link to the page being tested for this object</param>
+		public TestHelper(string URL)
+		{
+			this.URL = URL;
+		}
+
+		/// <summary>
+		/// Instantiates and returns a Chrome-based web driver to be used for Selenium testing.
+		/// Also, this function will open the page specified by this object's URL field
+		/// </summary>
+		/// <returns>RemoteWebDriver - the Chrome Driver to be used in Selenium testing</returns>
+		public RemoteWebDriver GetChromeDriver()
+		{
+			RemoteWebDriver driver;
+			driver = new ChromeDriver();
+			driver.Navigate().GoToUrl(URL);
+			return driver;
+		}
+	}
+}
