8ebff0704d9dee8337ba3c1c50fc321c1e2aa9d2
Bianca Aguirre <biancanoelaguirre@gmail.com>
2018-02-21 19:40:02 -0800

    Fuel Inventory Testing

--- BOSSMVC5.Tests/FuelInventoryTest.cs
+++ BOSSMVC5.Tests/FuelInventoryTest.cs
@@ -11,41 +11,120 @@ namespace BOSSMVC5.Tests
     [TestClass]
     public class FuelInventoryTest
     {
-        public RemoteWebDriver StartBrowser()
-        {
-            RemoteWebDriver driver;
-            string url = "http://teamjuxcoboss-env.us-west-1.elasticbeanstalk.com/";
-            driver = new ChromeDriver();
-            driver.Navigate().GoToUrl(url);
-            driver.Manage().Window.Maximize();
-            return driver;
-        }
-        
+        /// <summary>
+		/// Front end functionality tests -------------------------------------------------------------------------
+		/// </summary>
 
+
+        /// <summary>
+		/// Tests the "Tank A" link on Fuel Inventory screen
+		/// </summary>
         [TestMethod]
-        public void FuelInventoryLink()
+        public void OpenTankA()
         {
-            RemoteWebDriver driver = StartBrowser();
-            driver.FindElementByLinkText("Fuel Inventory").Click();
-            
+            TestHelper th = new TestHelper("http://teamjuxcoboss-env.us-west-1.elasticbeanstalk.com/FuelInventory/A/");
+            RemoteWebDriver chromeDriver = th.GetChromeDriver();
+           
+            chromeDriver.FindElementByLinkText("Tank A").Click();
+
+            th.QuitDriver();
         }
 
+        /// <summary>
+		/// Tests the "Tank B" link on Fuel Inventory screen
+		/// </summary>
+        [TestMethod]
+        public void OpenTankB()
+        {
+            TestHelper th = new TestHelper("http://teamjuxcoboss-env.us-west-1.elasticbeanstalk.com/FuelInventory/A/");
+            RemoteWebDriver chromeDriver = th.GetChromeDriver();
+
+            chromeDriver.FindElementByLinkText("Tank B").Click();
+            th.QuitDriver();
+        }
+
+        /// <summary>
+		/// Tests the Editing of Recent Table Input
+		/// </summary>
         [TestMethod]
         public void RecentTableInput()
         {
-            RemoteWebDriver driver = StartBrowser();
-            driver.FindElementByLinkText("Fuel Inventory").Click();
-            driver.FindElementById("February 7, 2018-display").Click();
-            driver.Keyboard.SendKeys("1.00");
-            driver.FindElementByName("Save").Click();
+            TestHelper th = new TestHelper("http://teamjuxcoboss-env.us-west-1.elasticbeanstalk.com/FuelInventory/A/");
+            RemoteWebDriver chromeDriver = th.GetChromeDriver();
+
+            chromeDriver.FindElementByXPath("//a[@href='#edit']").Click();
+            //chromeDriver.FindElementByPartialLinkText("gallons").Click();             //works as well
+            chromeDriver.Keyboard.SendKeys("12.00");
+            chromeDriver.FindElementByCssSelector("input.forminput.formsubmit").Click();
+            //th.QuitDriver();
         }
 
+        /// <summary>
+		/// Tests the Editing of Recent Table Input of Tank A
+		/// </summary>
         [TestMethod]
-        public void DashboardLink()
+        public void RecentTableInputTankA()
         {
-            RemoteWebDriver driver = StartBrowser();
-            driver.FindElementByLinkText("Fuel Inventory").Click();
+            TestHelper th = new TestHelper("http://teamjuxcoboss-env.us-west-1.elasticbeanstalk.com/FuelInventory/A/");
+            RemoteWebDriver chromeDriver = th.GetChromeDriver();
 
+            chromeDriver.FindElementByLinkText("Tank A").Click();
+            chromeDriver.FindElementByPartialLinkText("gallons").Click();
+            chromeDriver.Keyboard.SendKeys("12.00");
+            chromeDriver.FindElementByCssSelector("input.forminput.formsubmit").Click();
+            th.QuitDriver();
         }
+
+        /// <summary>
+		/// Tests the Editing of Recent Table Input of Tank B
+		/// </summary>
+        [TestMethod]
+        public void RecentTableInputTankB()
+        {
+            TestHelper th = new TestHelper("http://teamjuxcoboss-env.us-west-1.elasticbeanstalk.com/FuelInventory/A/");
+            RemoteWebDriver chromeDriver = th.GetChromeDriver();
+
+            chromeDriver.FindElementByLinkText("Tank B").Click();
+            chromeDriver.FindElementByPartialLinkText("gallons").Click();
+            chromeDriver.Keyboard.SendKeys("12.00");
+            chromeDriver.FindElementByCssSelector("input.forminput.formsubmit").Click();
+            th.QuitDriver();
+        }
+
+        /// <summary>
+		/// Tests the link to Individual Months in Past Year Table
+        /// 
+        /// Description: href will always be in form "YYYY-MM" Finds Element by looking for links that contain "-"
+        /// 
+		/// </summary>
+        [TestMethod]
+        public void OpenIndividualMonth()
+        {
+            TestHelper th = new TestHelper("http://teamjuxcoboss-env.us-west-1.elasticbeanstalk.com/FuelInventory/A/");
+            RemoteWebDriver chromeDriver = th.GetChromeDriver();
+            
+            chromeDriver.FindElementByXPath("//a[contains(@href, '-')]").Click();
+            th.QuitDriver();
+        }
+
+        /// <summary>
+		/// Tests editing of Individual month table
+		/// </summary>
+        [TestMethod]
+        public void IndividualMonthInput()
+        {
+            TestHelper th = new TestHelper("http://teamjuxcoboss-env.us-west-1.elasticbeanstalk.com/FuelInventory/A/2017-03");
+            RemoteWebDriver chromeDriver = th.GetChromeDriver();
+
+            chromeDriver.FindElementByPartialLinkText("gallons").Click();
+            chromeDriver.Keyboard.SendKeys("6.00");
+            chromeDriver.FindElementByCssSelector("input.forminput.formsubmit").Click();
+            th.QuitDriver();
+        }
+
+
+        /// <summary>
+		/// Back functionality tests -------------------------------------------------------------------------
+		/// </summary>
     }
 }
