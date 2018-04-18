b75054b3e217325fbc1d1acfa9b9220950d88e83
Jason4681 <jasonl4681@gmail.com>
2018-04-17 21:09:36 -0700

    wip

---
+++ MVC5App/Controllers/RestaruantPricesController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC5App.Models;
+
+namespace MVC5App.Controllers
+{
+    public class RestaruantPricesController : Controller
+    {
+        // GET: RestaruantPrices
+        public ActionResult RestaurantPrices()
+        {
+            var RestaurantPrices = new RestaruantPricesModels() { };
+
+            return View(RestaurantPrices);
+        }
+
+        public ActionResult GetPrices()
+        {
+            using (var sql = new Tools.OurSql())
+            {
+                List<RestaruantPricesModels> priceList = new List<RestaruantPricesModels>();
+                var rdr = sql.Query("");
+                while (rdr.Read())
+                {
+                    var priceData = new RestaruantPricesModels()
+                    {
+                        FoodName = rdr.GetString(0),
+                        FoodCategory = rdr.GetString(1),
+                        FoodId = rdr.GetDecimal(2),
+                        CostPrice = rdr.GetDecimal(3),
+                        ListPrice = rdr.GetDecimal(4)
+                    };
+                    priceList.Add(priceData);
+                }
+                return Json(new { priceList }, JsonRequestBehavior.AllowGet);
+            }
+
+        }
+
+        public void Update(string foodName, string foodCategory, Decimal foodId, Decimal costPrice, Decimal listPrice)
+        {
+
+        }
+
+        public void Insert(string foodName, string foodCategory, Decimal foodId, Decimal costPrice, Decimal listPrice)
+        {
+
+        }
+    }
+}
---
+++ MVC5App/Models/RestaruantPricesModels.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC5App.Models
+{
+    public class RestaruantPricesModels
+    {
+        public string FoodName { get; set; }
+        public string FoodCategory { get; set; }
+        public Decimal FoodId { get; set; }
+        public Decimal CostPrice { get; set; }
+        public Decimal ListPrice { get; set; }
+    }
+}
